9b4c13e7542cd6949b21edae79932bf2b985c9d0
Angel <angelhodar76@gmail.com>
2024-07-29 19:11:05 +0200

    Added server proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [TanStackRouterVite(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL as string,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
